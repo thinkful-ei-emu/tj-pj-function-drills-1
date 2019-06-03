756730700a43ca39145f3c90e159ab2d6ea4897e
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-03 13:37:52 -0500

    added yearofBirth

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 'use strict'
 function createGreeting(name, age){
-  return `Hi, my name is ${name} and I am ${age} years old.`;
+    let yearOfBirth = 2019-age;
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearofBirth}.`;
 }
 
 const greeting1 = createGreeting();
