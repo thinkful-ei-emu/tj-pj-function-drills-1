0350e15c0c2cb3f7f1f0242f52e7ba35049fd398
Prabhjot Singh <pjsingh1359@outlook.com>
2019-06-03 11:43:49 -0700

    added yearOfBirth function

--- functions.js
+++ functions.js
@@ -1,7 +1,14 @@
-'use strict'
+'use strict';
+
+function getYearOfBirth(age) {
+  let yearOfBirth = 2019-age;
+  return yearOfBirth;
+}
+
+
 function createGreeting(name, age){
-    let yearOfBirth = 2019-age;
-  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearofBirth}.`;
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting1 = createGreeting();
